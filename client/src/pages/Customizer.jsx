1e2e8aa384497277c287f3be50ab694b74f4a9e8
jaga172016 <jaga.101602@gmail.com>
2023-07-14 10:49:58 +0530

    Correction in Bankend end point address at Customizer file

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,7 +60,7 @@ const Customizer = () => {
 			setGeneratingImg(true);
 
 			const response = await fetch(
-				"http://https://project-de-zign.onrender.com/api/v1/dalle",
+				"https://project-de-zign.onrender.com/api/v1/dalle",
 				{
 					method: "POST",
 					headers: {
