e975b0d05918a8b36b65bee279ce02c7a465b2f5
jaga172016 <jaga.101602@gmail.com>
2023-07-14 11:11:39 +0530

    Reverses last commit

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,7 +60,7 @@ const Customizer = () => {
 			setGeneratingImg(true);
 
 			const response = await fetch(
-				"project-de-zign.onrender.com/api/v1/dalle",
+				"https://project-de-zign.onrender.com/api/v1/dalle",
 				{
 					method: "POST",
 					headers: {
