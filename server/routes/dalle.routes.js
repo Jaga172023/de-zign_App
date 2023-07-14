4cc8276621e059921fa044d3da6427c36ae47664
jaga172016 <jaga.101602@gmail.com>
2023-07-14 11:56:37 +0530

    Minor Commit

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -22,7 +22,7 @@ router.route("/").post(async (req, res) => {
 
 		const response = await openAI.createImage({
 			prompt,
-			n: 1,
+			no: 1,
 			size: "1024x1024",
 			response_format: "b64_json",
 		});
