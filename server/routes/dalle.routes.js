5ac219c665fdc9cc31e00fba8e039bfc7902599f
jaga172016 <jaga.101602@gmail.com>
2023-07-15 02:14:51 +0530

    Minor Correction

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,22 +7,22 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-	apiKey: process.env.OPENAI_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openAI = new OpenAIApi(config);
+const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
-	res.status(200).json({ message: "Hello from OpenAPI Routes!!" });
+	res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
 
 router.route("/").post(async (req, res) => {
 	try {
 		const { prompt } = req.body;
 
-		const response = await openAI.createImage({
+		const response = await openai.createImage({
 			prompt,
-			no: 1,
+			n: 1,
 			size: "1024x1024",
 			response_format: "b64_json",
 		});
